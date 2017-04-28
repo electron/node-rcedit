1551b6ddc08cae436895d6c01099ed9f3451dc68
Kevin Sawicki <kevinsawicki@gmail.com>
2017-04-28 11:33:54 -0700

    Log download/saving errors

--- script/download-latest-rcedit.js
+++ script/download-latest-rcedit.js
@@ -6,4 +6,9 @@ const path = require('path')
 
 const downloadURL = 'https://ci.appveyor.com/api/projects/Atom/rcedit/artifacts/Default/rcedit.exe?job=Image:%20Visual%20Studio%202015&branch=master'
 const filePath = path.join(__dirname, '..', 'bin', 'rcedit.exe')
+
+process.on('uncaughtException', (error) => {
+  console.log('Dowload failed:', error.message)
+})
+
 got.stream(downloadURL).pipe(fs.createWriteStream(filePath))
