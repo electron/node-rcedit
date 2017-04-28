e9a4dfa593359d2d81e1cd5dacf2c4c1d8f6b544
Kevin Sawicki <kevinsawicki@gmail.com>
2017-04-28 11:34:08 -0700

    Update URL for new account location

--- script/download-latest-rcedit.js
+++ script/download-latest-rcedit.js
@@ -4,7 +4,7 @@ const fs = require('fs')
 const got = require('got')
 const path = require('path')
 
-const downloadURL = 'https://ci.appveyor.com/api/projects/Atom/rcedit/artifacts/Default/rcedit.exe?job=Image:%20Visual%20Studio%202015&branch=master'
+const downloadURL = 'https://ci.appveyor.com/api/projects/electron-bot/rcedit/artifacts/Default/rcedit.exe?job=Image:%20Visual%20Studio%202015&branch=master'
 const filePath = path.join(__dirname, '..', 'bin', 'rcedit.exe')
 
 process.on('uncaughtException', (error) => {
