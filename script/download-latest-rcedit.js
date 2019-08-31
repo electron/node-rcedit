a2090b72d6927472f0b7b35c5f3c2295835619c4
Andrei Neculau <andrei.neculau@gmail.com>
2019-08-31 09:53:38 +0200

    add support for x64

--- script/download-latest-rcedit.js
+++ script/download-latest-rcedit.js
@@ -4,11 +4,15 @@ const fs = require('fs')
 const got = require('got')
 const path = require('path')
 
-const downloadURL = 'https://ci.appveyor.com/api/projects/zcbenz/rcedit/artifacts/Default/rcedit-x86.exe?job=Platform:%20Win32'
-const filePath = path.resolve(__dirname, '..', 'bin', 'rcedit.exe')
+const downloadURL32 = 'https://ci.appveyor.com/api/projects/zcbenz/rcedit/artifacts/Default/rcedit-x86.exe?job=Platform:%20Win32'
+const filePath32 = path.resolve(__dirname, '..', 'bin', 'rcedit.exe')
+
+const downloadURL64 = 'https://ci.appveyor.com/api/projects/zcbenz/rcedit/artifacts/Default/rcedit-x64.exe?job=Platform:%20x64'
+const filePath64 = path.resolve(__dirname, '..', 'bin', 'rcedit-x64.exe')
 
 process.on('uncaughtException', error => {
-  console.log('Downloading rcedit.exe failed:', error.message)
+  console.log('Downloading rcedit executables failed:', error.message)
 })
 
-got.stream(downloadURL).pipe(fs.createWriteStream(filePath))
+got.stream(downloadURL32).pipe(fs.createWriteStream(filePath32))
+got.stream(downloadURL64).pipe(fs.createWriteStream(filePath64))
