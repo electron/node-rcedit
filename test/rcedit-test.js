aaa2454975492a0fa4db7debb25066fff1ba383d
Mark Lee <electronjs@lazymalevolence.com>
2020-07-01 08:42:17 -0700

    test: assertRceditError is an async function, so await

--- test/rcedit-test.js
+++ test/rcedit-test.js
@@ -106,21 +106,21 @@ describe('async rcedit(exePath, options)', function () {
   })
 
   it('reports an error when the .exe path does not exist', async () => {
-    assertRceditError(path.join(tempPath, 'does-not-exist.exe'), { 'file-version': '3.4.5.6' }, [
+    await assertRceditError(path.join(tempPath, 'does-not-exist.exe'), { 'file-version': '3.4.5.6' }, [
       'rcedit.exe failed with exit code 1.',
       'Unable to load file'
     ])
   })
 
   it('reports an error when the icon path does not exist', async () => {
-    assertRceditError(exePath, { icon: path.join(tempPath, 'does-not-exist.ico') }, ['Fatal error: Unable to set icon'])
+    await assertRceditError(exePath, { icon: path.join(tempPath, 'does-not-exist.ico') }, ['Fatal error: Unable to set icon'])
   })
 
   it('reports an error when the file version is invalid', async () => {
-    assertRceditError(exePath, { 'file-version': 'foo' }, ['Fatal error: Unable to parse version string for FileVersion'])
+    await assertRceditError(exePath, { 'file-version': 'foo' }, ['Fatal error: Unable to parse version string for FileVersion'])
   })
 
   it('reports an error when the product version is invalid', async () => {
-    assertRceditError(exePath, { 'product-version': 'foo' }, ['Fatal error: Unable to parse version string for ProductVersion'])
+    await assertRceditError(exePath, { 'product-version': 'foo' }, ['Fatal error: Unable to parse version string for ProductVersion'])
   })
 })
