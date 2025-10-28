e06423895c129394e8530bd51daf05cdffef1e93
Erick Zhao <erick@hotmail.ca>
2025-10-28 13:25:09 -0700

    yarn 4

--- test/index.test-d.ts
+++ test/index.test-d.ts
@@ -4,6 +4,8 @@ import rcedit from '..'
 await rcedit('foo.exe', {})
 await rcedit('foo.exe', { 'version-string': { CompanyName: 'FooCorp' }, 'requested-execution-level': 'requireAdministrator' })
 
+// @ts-expect-error tests
 expectError(await rcedit('foo.exe', { 'version-string': { UnknownProperty: 'asdf' } }))
 
+// @ts-expect-error tests
 expectError(await rcedit('foo.exe', { 'requested-execution-level': 'unknown' }))
